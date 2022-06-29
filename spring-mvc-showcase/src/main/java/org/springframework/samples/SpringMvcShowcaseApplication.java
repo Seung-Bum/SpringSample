987000a750bd9afd97361ed0c8742e4a198cd835
Seung-Bum <sb910126@gmail.com>
2022-06-29 23:51:57 +0900

    메시지컨버터공부

--- spring-mvc-showcase/src/main/java/org/springframework/samples/SpringMvcShowcaseApplication.java
+++ spring-mvc-showcase/src/main/java/org/springframework/samples/SpringMvcShowcaseApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// 하위의 파일들을 모두 불러온다.
 public class SpringMvcShowcaseApplication {
 
 	public static void main(String[] args) {
